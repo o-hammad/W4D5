67a712c51cc27d7bd80eedb1875c57bb3d05266e
claudiaaziz <claudiahannahh@gmail.com>
2023-09-15 15:06:33 -0400

    Add my_transpose

--- TDD/TDD/lib/exercises.rb
+++ TDD/TDD/lib/exercises.rb
@@ -1,9 +1,38 @@
-def my_uniq(arr)
-    result = []
+class Array
 
-    arr.each do |ele|
-        result << ele if !result.include?(ele)
+    def my_uniq
+        result = []
+
+        self.each do |ele|
+            result << ele if !result.include?(ele)
+        end
+
+        result
     end
 
-    result
+    def two_sum
+        count = 0
+
+        self.each_with_index do |ele_1, idx_1|
+            self.each_with_index do |ele_2, idx_2|
+                count += 1 if idx_1 < idx_2 && ele_1 + ele_2 == 0
+            end
+        end
+
+        count
+    end
+
+    def my_transpose
+        result = []
+
+        (0...self.length).each do |row|
+            subarr = []
+            (0...self.length).each do |col|
+                subarr << self[col][row]
+            end
+            result << subarr
+        end
+
+        result
+    end
 end
--- TDD/TDD/spec/exercises_spec.rb
+++ TDD/TDD/spec/exercises_spec.rb
@@ -4,9 +4,28 @@ require "exercises"
 describe "#my_uniq" do
 
     it "should accept an array as an arg" do 
-        expect { my_uniq([1, 2, 1, 3, 3]) }.to_not raise_error
+        expect { [1, 2, 1, 3, 3].my_uniq }.to_not raise_error
     end
     it "should return a unique array" do
-        expect( my_uniq([1, 2, 1, 3, 3]) ).to eq([1, 2, 3])
+        expect( [1, 2, 1, 3, 3].my_uniq ).to eq([1, 2, 3])
+    end
+end
+
+describe "#two_sum" do 
+    it "should accept an array as an arg" do
+        expect { [-1, 0, 2, -2, 1].two_sum }.to_not raise_error
+    end
+    it "should return the number of pairs of elements that sum to zero" do
+        expect([-1, 0, 2, -2, 1].two_sum).to eq(2)
+    end
+end
+
+describe "#my_transpose" do
+    it "should accept an array as an arg" do 
+        expect { [[0, 1, 2],[3, 4, 5],[6, 7, 8]].my_transpose }.to_not raise_error
+    end
+
+    it "should return a transposed array" do 
+        expect([[0, 1, 2],[3, 4, 5],[6, 7, 8]].my_transpose).to eq([[0, 3, 6],[1, 4, 7],[2, 5, 8]])
     end
 end
